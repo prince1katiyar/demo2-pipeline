65756ee17cc28fe3ccd1e5563383a67718563147
prince katiyar <princekatiyar986@gmail.com>
2023-03-27 15:22:11 +0530

    heloo

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import  setup, find_packages
+
+setup(name="census2-income",
+      version="0.0.1",
+      author="prince",
+      author_email="princekatiyar986@gmail.com",
+      pakages=find_packages(),
+      install_require=["pandas","numpy","flask"]
+
+
+
+)
